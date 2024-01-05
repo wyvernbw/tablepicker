abb12c15b493db8e07bc60471cc39608ae8c06e3
wyvernbw <iamwyvern511@gmail.com>
2024-01-05 17:29:35 +0200

    add db glue code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,9 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
-      },
+      //fontFamily: {
+      //  sans: ["var(--font-sans)", ...fontFamily.sans],
+      //},
     },
   },
   plugins: [],
